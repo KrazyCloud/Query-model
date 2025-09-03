bb293fcd5998694d6dd26d304f69f6b44d3586c8
KrazyCloud <karan.bisht1915@gmail.com>
2025-09-03 17:16:21 +0530

    update the port to 0.0.0.0

--- search_agent.py
+++ search_agent.py
@@ -150,4 +150,4 @@ def agent_pipeline(request: QueryRequest):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="10.226.54.4", port=9000)
+    uvicorn.run(app, host="0.0.0.0", port=9000)
